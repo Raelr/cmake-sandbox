2839455453f4af77570e6c0ba8e5a32fa63bd1e0
Aryeh <aryehzinn@hotmail.com>
2020-05-13 07:19:48 +1000

    Added symbol introspection for CMAKE

--- TestLib/test.cpp
+++ TestLib/test.cpp
@@ -1,6 +1,14 @@
 #include "test.h"
 #include <iostream>
+#include <cmath>
+#include "ProjectA.h"
 
 void test() {
-    std::cout << "Test" << std::endl;
+
+    // Run specific code if and ONLY if the symbols required actually exist. 
+    #if defined(HAVE_LOG) && defined(HAVE_EXP)
+        std::cout << "Test with exp and log" << std::endl;
+    #else
+        std::cout << "Test without exp and log!" << std::endl;
+    #endif
 }
